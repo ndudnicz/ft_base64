fdfcde0bc90000951096528d7924137be1aebdb4
ndudnicz <ndudnicz@protonmail.com>
2018-05-10 10:31:23 +0200

    base64_encode done, add readme

---
+++ README.md
@@ -0,0 +1,11 @@
+## Base64 encode/decode implementation in C
+
+#### Encode:
+```C
+char* base64_encode(void* data, size_t size)
+```
+
+#### Decode
+```C
+
+```
--- base64.c
+++ base64.c
@@ -2,6 +2,14 @@
 #include <string.h>
 #include <stdio.h>
 
+/*
+** Bytes:                A               B               C
+**               |_ _ _ _ _ _ _ _|_ _ _ _ _ _ _ _|_ _ _ _ _ _ _ _|
+**               |_ _ _ _ _ _|_ _|_ _ _ _|_ _ _ _|_ _|_ _ _ _ _ _|
+** Base64           A_LEFT     ^  B_LEFT     ^   C_LEFT    ^
+**  masks:                  A_RIGHT       B_RIGHT       C_RIGHT
+*/
+
 #define A_LEFT 0xFC
 #define A_RIGHT 0x03
 
@@ -11,99 +19,85 @@
 #define C_LEFT 0xC0
 #define C_RIGHT 0x3F
 
-#define LENGTH(X) (X * 15 / 10)
+/* get the base64 final string length */
+#define BASE64_LENGTH(X) (((X - 1) / 3) + X + 1 + (X%3 ? (X%3 == 1 ? 2 : 1) : 0))
 
 static const char arr[64] = {
-	'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-	'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-	'0','1','2','3','4','5','6','7','8','9','+','/'
+	'A','B','C','D','E','F','G','H','I','J',
+	'K','L','M','N','O','P','Q','R','S','T',
+	'U','V','W','X','Y','Z','a','b','c','d',
+	'e','f','g','h','i','j','k','l','m','n',
+	'o','p','q','r','s','t','u','v','w','x',
+	'y','z','0','1','2','3','4','5','6','7',
+	'8','9','+','/'
 };
 
-
+/*
+** Take a data array and its size as parameters, calculate the final base64
+** string and convert it into base64.
+** Return a malloced char * array.
+** Possible improvement: remove switch/case and for loop by +=3
+*/
 char*
 base64_encode(
 	void* data,
 	size_t size
 )
 {
-	char		*base64_data = (char*)calloc(1, LENGTH(size) + 1);
+	char		*base64_data;
 	uint8_t		*data_uint8 = (uint8_t*)data;
 	uint8_t		remain = 0;
 	size_t		i, n;
 
-	for (i = 0, n=0; i < size - 2; i+=3, n+=3) {
-		// switch (i % 3) {
-		// 	case 0:
-		// 	printf("A");
-		// 	base64_data[n] = arr[ (data_uint8[i] & A_LEFT) >> 2 ];
-		// 	remain = data_uint8[i] & A_RIGHT;
-		// 	break;
-        //
-		// 	case 1:
-		// 	printf("B");
-		// 	base64_data[n] = arr[ ((data_uint8[i] & B_LEFT) >> 4) | (remain << 4) ];
-		// 	remain = data_uint8[i] & B_RIGHT;
-		// 	break;
-        //
-		// 	case 2:
-		// 	printf("C");
-		// 	base64_data[n] = arr[ ((data_uint8[i] & C_LEFT) >> 6) | (remain << 2) ];
-		// 	n++;
-		// 	base64_data[n] = arr[ data_uint8[i] & C_RIGHT ];
-		// 	break;
-        //
-		// 	default:
-		// 	break;
-		// }
-		base64_data[n] = arr[ (data_uint8[i] & A_LEFT) >> 2 ];
-		remain = data_uint8[i] & A_RIGHT;
-
-		// if (i + 1 < size) {
-			base64_data[n+1] = arr[ ((data_uint8[i+1] & B_LEFT) >> 4) | (remain << 4) ];
-			remain = data_uint8[i] & B_RIGHT;
-		// }
-
-		// if (i + 2 < size) {
-			base64_data[n+2] = arr[ ((data_uint8[i+2] & C_LEFT) >> 6) | (remain << 2) ];
-			n++;
-			base64_data[n+2] = arr[ data_uint8[i+2] & C_RIGHT ];
-		// }
-		// break;
-
+	if (!(base64_data = (char*)calloc(1, BASE64_LENGTH(size) + 1))) {
+		return NULL;
 	}
-	printf("i:%d n:%d\n",i, n );
-	// i-=3;
-	// n-=3;
-	if (i + 1 < size) {
-		printf("A\n");
-		base64_data[n+1] = arr[ ((data_uint8[i+1] & B_LEFT) >> 4) | (remain << 4) ];
-		remain = data_uint8[i+1] & B_RIGHT;
-	}
+	else {
+		for (i = 0, n = 0; i < size; i += 1, n += 1) {
+			switch (i % 3) {
+				case 0 /* A */ :
+				base64_data[n] = arr[ (data_uint8[i] & A_LEFT) >> 2 ];
+				remain = data_uint8[i] & A_RIGHT;
+				break;
 
-	if (i + 2 < size) {
-		printf("B\n");
-		base64_data[n+2] = arr[ ((data_uint8[i+2] & C_LEFT) >> 6) | (remain << 2) ];
-		n++;
-		base64_data[n+2] = arr[ data_uint8[i+2] & C_RIGHT ];
-	}
+				case 1 /* B */ :
+				base64_data[n] = arr[ ((data_uint8[i] & B_LEFT) >> 4) | (remain << 4) ];
+				remain = data_uint8[i] & B_RIGHT;
+				break;
 
-	if (size % 3) {
-		base64_data[n+2] = arr[ size % 3 == 1 ? remain << 4 : remain << 2 ];
+				case 2 /* C */ :
+				base64_data[n] = arr[ ((data_uint8[i] & C_LEFT) >> 6) | (remain << 2) ];
+				n++;
+				base64_data[n] = arr[ data_uint8[i] & C_RIGHT ];
+				break;
+
+				default:
+				break;
+			}
+		}
+		if (size % 3 == 1) {
+			base64_data[n] = arr[ remain << 4 ];
+			base64_data[n+1] = '=';
+			base64_data[n+2] = '=';
+		} else if (size % 3 == 2) {
+			base64_data[n] = arr[ remain << 2 ];
+			base64_data[n+1] = '=';
+		}
+		return base64_data;
 	}
-	return base64_data;
 }
 
-char*
+void*
 base64_decode(
-	void* data,
-	size_t size
+	char* string
 )
 {
+	size_t const	string_len = strlen(string);
 	return NULL;
 }
 
 int		main(int ac, char **av)
 {
-	printf("\n%s\n", base64_encode(av[1], strlen(av[1])));
+	printf("%s\n", base64_encode(av[1], strlen(av[1])));
 	return (0);
 }
